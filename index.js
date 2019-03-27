fa3e34c5ffa9165f7f28b7bfd5d8d6173fdb4588
Damian Randall <damian.randall@blackrock.com>
2019-03-27 21:25:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,6 +34,6 @@ function johnLennonFacts () {
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
-  "He hated the sound of his own voice")
+  "He hated the sound of his own voice");
 }
 johnLennonFacts();
