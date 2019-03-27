69fa38f57e0b9c295961ae95a0af5c22e74aa764
Damian Randall <damian.randall@blackrock.com>
2019-03-27 21:19:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
         
         
         
-        "He hated the sound of his own voice"
+        
       ])).toEqual(["He was the last Beatle to learn to drive!!!", "He was never a vegetarian!!!", "He was a choir boy and boy scout!!!", "He hated the sound of his own voice!!!"]);
 
       expect(johnLennonFacts([
